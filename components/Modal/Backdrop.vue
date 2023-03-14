1ebefba177d1a6e440c3100e266084303b322003
Nathan <asnpldev@gmail.com>
2023-03-15 00:02:46 +0100

    feat: modal setup

---
+++ components/Modal/Backdrop.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="Backdrop">
+
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style lang="scss" scoped>
+.Backdrop {
+  background: rgba(52, 64, 84, 0.7);
+  backdrop-filter: blur(8px);
+
+}
+</style>
---
+++ components/Modal/Modal.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="Modal">
+
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style lang="scss" scoped>
+.Modal {
+}
+</style>
