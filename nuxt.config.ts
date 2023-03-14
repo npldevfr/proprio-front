da8547ef5a3af8dfa453a2bc1d67e8a17db5237a
Nathan <asnpldev@gmail.com>
2023-03-14 16:33:15 +0100

    style(reset): added reset style

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     modules: ['nuxt-icon', 'nuxt-vitest'],
+
+    // @ts-ignore
+    css: [
+        '~/assets/scss/reset.scss'
+    ],
 })
