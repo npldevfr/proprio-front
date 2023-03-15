0280d8b39cc9be33127fe976a4cf3fa34958c732
asnpldev <asnpldev@gmail.com>
2023-03-15 11:30:27 +0100

    story: toast

---
+++ components/Toast/Toast.story.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <Story title="Toast" :layout="{type: 'grid', width: '40%'}" icon="mdi:toaster">
+
+    <Variant title="Default">
+      <Toast :toast-props="{data: { content: 'hello world' }}"/>
+    </Variant>
+  </Story>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,9 @@
 <script lang="ts" setup>
-const userAvatar = reactive({
-  username: "Nathan Guillet",
-  avatar_url: "https://avatars.githubusercontent.com/u/10214025?v=4",
-  text: true,
-  size: "md",
-});
-
-const status = reactive({
-  status: "online",
-});
+const {toast} = useToast()
 </script>
 
 <template>
-  <UserAvatar :username="userAvatar.username" :avatar_url="userAvatar.avatar_url"
-              :text="userAvatar.text" :size="userAvatar.size">
-    <StatusIndicator :status="status.status" :size="userAvatar.size"/>
-  </UserAvatar>
+  <Button @action="toast('hello my friend', 2000)">
+    click me
+  </Button>
 </template>
